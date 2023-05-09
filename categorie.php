5f1d973181239f82666737beb5f9a8f92ff922d4
julespace <jules.broto@limayrac.fr>
2023-05-09 23:09:06 +0200

    categorie

--- categorie.php
+++ categorie.php
@@ -2,28 +2,33 @@
 <?php
 require('function/function.php');
 $id = isset($_GET['id']) ? $_GET['id'] : NULL;
-$sel=$dbh->prepare("SELECT * FROM produit  WHERE :id=produit.id_cat");
-$sel->execute(array(
-    ':id' => $id
-));
 ?>
 <div id="index_cate" class="index_cate-section index-section">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <ul class="product-list">
 <?php
-if($id==1){
-    echo'<ul class="product-list">';
-        echo '<li class="incate product-item">';
-            echo'<a href="categorie.php?id=1" class="product-list-content" title="chaise">';
-                echo'<div class="listimg"><img src="img\chaise.jpg" border="0" alt="KIOSK" /></div>';
-                echo'<div class="list-name">Chaise retournée</div>';
-                echo'<div class="prolistdesc"></div>';
-            echo'</a>';
-        echo'</li>';                          
-    echo'</ul>';
+try{
+    $sel=$dbh->prepare("SELECT * FROM produit  WHERE :id=produit.id_cat");
+    $sel->execute(array(
+    ':id' => $id
+    ));
+    $produits = $sel->fetch(PDO::FETCH_ASSOC);
+}catch(PDOException $ex){
+    die("Erreur lors de la requête SQL : " . $ex->getMessage());
 }
-?>
+    foreach($produits AS $produit){
+                    echo'<li class="incate product-item">';
+                        echo'<a href="produit.php?id='.$produit['id_produit'].'" class="product-list-content" title="Chaise">';
+                            echo'<div class="listimg"><img src="img\chaise'.$produit['id_produit'].'.jpg" border="0" alt="chaise" /></div>';
+                            echo'<div class="list-name">'.$produit['lib_produit'].'</div>';
+                            echo'<div class="prolistdesc"></div>';
+                        echo'</a>';
+                    echo'</li>';
+    }
+?>            
+                </ul>
             </div>
         </div>
     </div>
