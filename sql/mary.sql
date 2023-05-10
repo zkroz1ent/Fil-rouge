4f810114d7362597b01895dbcd8e5960952b5da4
julespace <jules.broto@limayrac.fr>
2023-05-10 22:36:38 +0200

    FiniCat4

--- sql/mary.sql
+++ sql/mary.sql
@@ -2,11 +2,11 @@ INSERT INTO produit (id_produit, lib_produit_fr, lib_produit, prix_produit, stoc
 VALUES 
 (31,'Pichasse', 'Bitch', 9.99, 50, 'Cette humeur se caractérise par une sensation de poser pour des photos instas.', 'This mood is characterized by a feeling of bitch.', 1, 4),
 (32,'Pichasse 2.0', 'Bitch 2.0', 9.99, 30, 'Cette humeur se caractérise par une sensation de poser pour des photos instas.', 'This mood is characterized by a feeling of bitch.', 1, 4),
-(33,'Colérique', 'Angry', 9.99, 20, 'Cette humeur se caractérise par une sensation de colère et de frustration.', 'This mood is characterized by a feeling of anger and frustration.', 1, 4),
+(33,'Colérique/Dadou', 'Angry/Dadou', 9.99, 20, 'Cette humeur se caractérise par une sensation de colère et de frustration.', 'This mood is characterized by a feeling of anger and frustration.', 1, 4),
 (34,'Apaisée', 'Calm', 9.99, 40, 'Cette humeur se caractérise par une sensation de calme et de relaxation.', 'This mood is characterized by a feeling of calmness and relaxation.', 1, 4),
-(35,'Ennuyée', 'Bored', 9.99, 25, 'Cette humeur se caractérise par une sensation d ennui et de désintérêt.', 'This mood is characterized by a feeling of boredom and disinterest.', 1, 4),
+(35,'Cracra', 'Cracra', 9.99, 25, 'Cette humeur se caractérise par une sensation DE MANQUE DE DOUCHE CA PUE', 'This mood is characterized by a feeling of stinky poop', 1, 4),
 (36,'Amoureuse', 'In Love', 9.99, 35, 'Cette humeur se caractérise par une sensation d amour et de passion.', 'This mood is characterized by a feeling of love and passion.', 1, 4),
-(37,'Stressée', 'Stressed', 9.99, 15, 'Cette humeur se caractérise par une sensation de stress et de tension.', 'This mood is characterized by a feeling of stress and tension.', 1, 4),
-(38,'Fatiguée', 'Tired', 9.99, 10, 'Cette humeur se caractérise par une sensation de fatigue et d épuisement.', 'This mood is characterized by a feeling of fatigue and exhaustion.', 1, 4),
+(37,'Boudine/Connasse', 'Boudine/Connasse', 9.99, 15, 'Cette humeur se caractérise par une sensation de bouder et être connasse', 'This mood is characterized by a feeling of being lazy and connasse.', 1, 4),
+(38,'Princesse', 'Princess', 9.99, 10, 'Cette humeur se caractérise par une sensation de richesse et de grand pouvoir.', 'This mood is characterized by a feeling of wealth and power.', 1, 4),
 (39,'Enthousiaste', 'Excited', 9.99, 45, 'Cette humeur se caractérise par une sensation d enthousiasme et d excitation.', 'This mood is characterized by a feeling of enthusiasm and excitement.', 1, 4),
 (40,'Déprimée', 'Depressed', 9.99, 5, 'Cette humeur se caractérise par une sensation de dépression et de désespoir.', 'This mood is characterized by a feeling of depression and despair.', 1, 4);
