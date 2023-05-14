0fb37380d9d2ffcdebb8139adf8639e615130d48
damien <damien.dutertre@limayrac.fr>
2023-05-14 21:52:09 +0200

    panier

--- panier.php
+++ panier.php
@@ -1,74 +1,74 @@
 <?php require 'header.php';  ?>
-
+<h1>Bonjour <?= $_SESSION['user']['pseudo']; ?></h1>
 
 <?php
 
 print_r($_SESSION);
 // Ajouter un produit au panier
-if (isset($_POST['add_to_cart'])) {
-    $product_id = $_POST['product_id'];
+if (isset($_POST['add_to_panier'])) {
+    $id_produit = $_POST['id_produit'];
     $product_name = $_POST['product_name'];
-    $product_price = $_POST['product_price'];
-    $quantity = $_POST['quantity'];
+    $prix_produit = $_POST['prix_produit'];
+    $quantite = $_POST['quantite'];
     // Si le panier n'existe pas encore, le créer
-    if (!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = array();
+    if (!isset($_SESSION['panier'])) {
+        $_SESSION['panier'] = array();
     }
     // Vérifier si le produit est déjà dans le panier, si oui, augmenter la quantité, sinon, l'ajouter au panier
     $product_found = false;
-    foreach ($_SESSION['cart'] as $key => $product) {
-        if ($product['product_id'] == $product_id) {
-            $_SESSION['cart'][$key]['quantity'] += $quantity;
+    foreach ($_SESSION['panier'] as $key => $product) {
+        if ($product['id_produit'] == $id_produit) {
+            $_SESSION['panier'][$key]['quantite'] += $quantite;
             $product_found = true;
             break;
         }
     }
-    
+
     if (!$product_found) {
-        $_SESSION['cart'][] = array(
-            'product_id' => $product_id,
-            'product_name' => $product_name,
-            'product_price' => $product_price,
-            'quantity' => $quantity
+        $_SESSION['panier'][] = array(
+            'id_produit' => $id_produit,
+            'pseudo' => $pseudo,
+            'prix_produit' => $prix_produit,
+            'quantite' => $quantite
         );
     }
 }
 // Supprimer un produit du panier
-if (isset($_POST['remove_from_cart'])) {
-    $product_id = $_POST['product_id'];
-    
-    foreach ($_SESSION['cart'] as $key => $product) {
-        if ($product['product_id'] == $product_id) {
-            unset($_SESSION['cart'][$key]);
+if (isset($_POST['remove_from_panier'])) {
+    $id_produit = $_POST['id_produit_delete'];
+
+    foreach ($_SESSION['panier'] as $key => $product) {
+        if ($product['id_produit'] == $id_produit) {
+            unset($_SESSION['panier'][$key]);
             break;
         }
     }
 }
 // Afficher le contenu du panier
 $total_price = 0;
-if (isset($_SESSION['cart'])) {
+if (isset($_SESSION['panier'])) {
     echo '<table>';
     echo '<tr><th>Produit</th><th>Prix</th><th>Quantité</th><th>Action</th></tr>';
-    foreach ($_SESSION['cart'] as $product) {
+    foreach ($_SESSION['panier'] as $product) {
         echo '<tr>';
-        echo '<td>'.$product['product_name'].'</td>';
-        echo '<td>'.$product['product_price'].'€</td>';
-        echo '<td>'.$product['quantity'].'</td>';
+        echo '<td>' . $product['lib_produit'] . '</td>';
+        echo '<td>' . $product['prix_produit'] . '€</td>';
+        echo '<td>' . $product['quantite'] . '</td>';
         echo '<td>';
         echo '<form method="post" action="">';
-        echo '<input type="hidden" name="product_id" value="'.$product['product_id'].'">';
-        echo '<input type="submit" name="remove_from_cart" value="Supprimer">';
+        echo '<input type="hidden" name="id_produit_delete" value="' . $product['id_produit'] . '">';
+        echo '<input type="submit" name="remove_from_panier" value="Supprimer">';
         echo '</form>';
         echo '</td>';
         echo '</tr>';
-        
-        $total_price += $product['product_price'] * $product['quantity'];
+
+        $total_price += $product['prix_produit'] * $product['quantite'];
     }
-    echo '<tr><td colspan="3">Total:</td><td>'.$total_price.'€</td></tr>';
+    echo '<tr><td colspan="3">Total:</td><td>' . $total_price . '€</td></tr>';
     echo '</table>';
 } else {
     echo 'Votre panier est vide.';
 }
 ?>
-<h1>Bonjour <?= $_SESSION['user']['pseudo'] ;?></h1>
+
 <?php require 'footer.php';  ?>
